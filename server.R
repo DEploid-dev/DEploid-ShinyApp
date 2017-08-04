af22684c3b7caed4ac27817246b7bc55606d2cb6
Joe Zhu <sha.joe.zhu@gmail.com>
2017-08-04 14:20:14 +0100

    update app

--- server.R
+++ server.R
@@ -2,8 +2,8 @@ rm(list=ls())
 library(quantmod)
 library(RCurl)
 
-# allow maximum vcf upload to 30mb
-options(shiny.maxRequestSize=30*1024^2)
+# allow maximum vcf upload to 100mb
+options(shiny.maxRequestSize=100*1024^2)
 
 
 # source("plaf.get.R")
@@ -42,7 +42,7 @@ deconvolutedGlobal = NULL
 
 
 letsTrimPlafVcf <- function (coverageVCF, plafFile) {
-
+  cat("Log: trim VCF and plaf")
   coverageVCF$MATCH <- paste(coverageVCF$CHROM, coverageVCF$POS, sep = "-")
   plafFile$MATCH <- paste(plafFile$CHROM, plafFile$POS, sep = "-")
   ### assertion: take a look at r-package: assertthat
@@ -260,15 +260,10 @@ function(input, output, session) {
 
   output$panelDataTotalCoverage <- renderDygraph({
     if (is.null(input$inputVCFfile)){
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
       return(NULL)
-    } else if (!isBothPlafVcfTrimmed){
-      if (is.null(coverageUntrimmedGlobal)){
-        stop("coverage should have been loaded")
-      }
-
-      if (is.null(plafUntrimmedGlobal)){
-        stop("plaf should have been loaded")
-      }
     }
     cat ("log: panelDataTotalCoverage\n")
     print(head(coverageTrimmedGlobal))
@@ -281,7 +276,10 @@ function(input, output, session) {
 
   output$panelDataAltVsRef <- renderPlotly({
     if (is.null(input$inputVCFfile)){
-      return (NULL)
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
+      return(NULL)
     }
 
     if (is.null(coverageTrimmedGlobal)){
@@ -308,7 +306,10 @@ function(input, output, session) {
 
   output$panelDataHistWSAF <- renderPlotly({
     if (is.null(input$inputVCFfile)){
-      return (NULL)
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
+      return(NULL)
     }
 
     if (is.null(coverageTrimmedGlobal)){
@@ -337,7 +338,10 @@ function(input, output, session) {
 
   output$panelDataWSAFVsPLAF <- renderPlotly({
     if (is.null(input$inputVCFfile)){
-      return (NULL)
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
+      return(NULL)
     }
 
     if (is.null(input$inputSample)){
@@ -388,6 +392,12 @@ function(input, output, session) {
 
 
   output$panelSequenceDeconWSAFVsPOS <- renderDygraph ({
+    if (is.null(input$inputVCFfile)){
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
+      return(NULL)
+    }
 #     deconvolute()
      if (is.null(deconvolutedGlobal)){
        return(NULL)
@@ -465,7 +475,10 @@ function(input, output, session) {
 
 
   output$panelSequenceDeconObsVsExpWSAF <- renderPlotly({
-    if (is.null(deconvolutedGlobal)){
+    if (is.null(input$inputVCFfile)){
+      validate(
+        need(input$inputVCFfile != "", "Please provide a VCF file")
+      )
       return(NULL)
     }
     deconvolutionIsCompleted <- TRUE
@@ -496,8 +509,8 @@ function(input, output, session) {
     progress <- Progress$new(session, min=1, max=15)
     on.exit(progress$close())
 
-    progress$set(message = 'Deconvolution in progress',
-                 detail = 'This may take a while...')
+    progress$set(message = "Deconvolution in progress, ",
+                 detail = "this may take a few mins ...")
 
     fetchPLAF()
     fetchVCF()
@@ -510,13 +523,13 @@ function(input, output, session) {
   })
 
 
-  observe({
-    if (is.null(deconvolutedGlobal)){
-      shinyjs::disable("downloadHaplotypes")
-    } else {
-      shinyjs::enable("downloadHaplotypes")
-    }
-  })
+#  observe({
+#    if (is.null(deconvolutedGlobal) | (is.null(input$inputVCFfile))){
+#      shinyjs::disable("downloadHaplotypes")
+#    } else {
+#      shinyjs::enable("downloadHaplotypes")
+#    }
+#  })
 
   output$downloadHaplotypes <- downloadHandler(
     filename = function() {
--- ui.R
+++ ui.R
@@ -29,7 +29,7 @@ fluidPage(
                    accept = c("text", "ref", "txt")),
           actionButton("do", "DEploid"),
           hr(),
-          HTML("TODO: advanced parameter turning"),
+          HTML("<font color=\"blue\">TODO: advanced parameter turning.</font>"),
           hr(),
           htmlOutput("panelSampleInfoExplainSample")
         ),
@@ -44,76 +44,77 @@ fluidPage(
     tabPanel("Sequence exploration",
       sidebarLayout(
         sidebarPanel(width = 2,
-          HTML("TODO: advanced parameter turning")
+          HTML("<font color=\"blue\">TODO: interactive buttons.</font>")
         ),
         mainPanel(width = 10,
           shinyjs::useShinyjs(),
           tabsetPanel(id = "data",
-            tabPanel("total coverage", value = "p1",
+            tabPanel("Coverage distribution across the genome", value = "p1",
               column(12, dygraphOutput("panelDataTotalCoverage"))
             ),
-            tabPanel("Alt VS Ref", value = "p2",
-              column(5, align = "center", plotlyOutput("panelDataAltVsRef"))
+            tabPanel("Allele read counts", value = "p3",
+              column(2),
+              column(6, align = "center", plotlyOutput("panelDataAltVsRef"))
+#              htmlOutput("panelDataExplainTotalCoverage"),
             ),
             tabPanel("WSAF Histogram", value = "p3",
+              column(3),
               column(5, align = "center", plotlyOutput("panelDataHistWSAF"))
+#              htmlOutput("panelDataExplainHistWSAF"),
             ),
             tabPanel("WSAF VS PLAF", value = "p4",
+              column(3),
               column(5, align = "center", plotlyOutput("panelDataWSAFVsPLAF"))
+#              htmlOutput("panelDataExplainWSAFVsPLAF")
             )
           )
         )
       )
-
-#      htmlOutput("serverDataState"),
-#      ### 1. total coverage
-#      fluidRow(column(1), column(10, dygraphOutput("panelDataTotalCoverage"))),
-#      htmlOutput("panelDataExplainTotalCoverage"),
-#      ### 2. Alt VS Ref
-#      fluidRow(column(4), column(5, align = "center",
-#        plotlyOutput("panelDataAltVsRef"))), #, height="550px", width="550px"
-#      htmlOutput("panelDataExplainAltVsRef"),
-#      ### 3. WSAF Histogram
-#      fluidRow(column(4), column(4, align = "center",
-#        plotlyOutput("panelDataHistWSAF"))),
-#      htmlOutput("panelDataExplainHistWSAF"),
-#      ### 4. WSAF VS PLAF
-#      fluidRow(column(4), column(4, align = "center",
-#        plotlyOutput("panelDataWSAFVsPLAF"))),
-#      htmlOutput("panelDataExplainWSAFVsPLAF")
     ),
 
     ############ tabPanel 3
     tabPanel("Sequence deconvolution",
-      downloadButton("downloadHaplotypes", "Download haplotypes"),
-      hr(),
+      sidebarLayout(
+        sidebarPanel(width = 2,
+          downloadButton("downloadHaplotypes", "Download haplotypes"),
+          HTML("<font color=\"blue\">TODO: interactive buttons.</font>")
+        ),
+        mainPanel(width = 10,
+          shinyjs::useShinyjs(),
+          tabsetPanel(id = "deconvolution",
+            tabPanel("Allele frequency across the genome", value = "p1",
+              selectInput("panelSequenceDeconSelectCHROM",
+                label = h4("Choose a CHROMOSOME"),
+                choices = list("Pf3D7_01_v3" = 1, "Pf3D7_02_v3" = 2, "Pf3D7_03_v3" = 3,
+                             "Pf3D7_04_v3" = 4, "Pf3D7_05_v3" = 5, "Pf3D7_06_v3" = 6,
+                             "Pf3D7_07_v3" = 7, "Pf3D7_08_v3" = 8, "Pf3D7_09_v3" = 9,
+                             "Pf3D7_10_v3" = 10, "Pf3D7_11_v3" = 11, "Pf3D7_12_v3" = 12,
+                             "Pf3D7_13_v3" = 13, "Pf3D7_14_v3" = 14),
+                selected = c(1)),
 
-      htmlOutput("severDeconvolutionState"),
-
-      selectInput("panelSequenceDeconSelectCHROM",
-        label = h4("Choose a CHROMOSOME"),
-        choices = list("Pf3D7_01_v3" = 1, "Pf3D7_02_v3" = 2, "Pf3D7_03_v3" = 3,
-                     "Pf3D7_04_v3" = 4, "Pf3D7_05_v3" = 5, "Pf3D7_06_v3" = 6,
-                     "Pf3D7_07_v3" = 7, "Pf3D7_08_v3" = 8, "Pf3D7_09_v3" = 9,
-                     "Pf3D7_10_v3" = 10, "Pf3D7_11_v3" = 11, "Pf3D7_12_v3" = 12,
-                     "Pf3D7_13_v3" = 13, "Pf3D7_14_v3" = 14),
-        selected = c(1)),
-      dygraphOutput("panelSequenceDeconWSAFVsPOS"),
-      htmlOutput("panelSequenceDeconExplainWSAFVsPOS"),
-      hr(),
-      fluidRow(column(4), column(4, align = "center",
-        plotlyOutput("panelSequenceDeconObsVsExpWSAF"))),
-      hr(),
-      hr()
+              column(12, dygraphOutput("panelSequenceDeconWSAFVsPOS"))
+            ),
+            tabPanel("Expected vs observed allele frequency", value = "p1",
+              column(4), column(4, align = "center",
+              plotlyOutput("panelSequenceDeconObsVsExpWSAF"))),
+            tabPanel("MCMC diagnostic",
+              htmlOutput("severMcMcState"),
+              tabsetPanel(id = "mcmc",
+                tabPanel("Proportions",
+                  fluidRow(
+                    column(2)#,
+#                    column(5, align = "center", plotlyOutput("panelMCMCProportions")
+                  )
+                ),
+                tabPanel("LLKs")
+              )
+            )
+          )
+        )
+      )
     ),
 
     ############ tabPanel 4
-    tabPanel("MCMC diagnostic",
-      htmlOutput("severMcMcState"),
-      fluidRow(column(3),
-        column(5, align = "center", plotlyOutput("panelMCMCProportions")))
-    ),
-
     ############ tabPanel 5
     navbarMenu("Documentation",
       tabPanel("Sample Infos"),
